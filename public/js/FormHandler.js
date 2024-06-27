8f835db419473223bdb1cd61c1e66b637f3efbc8
nitishk@instaacoders.com <nitishk@instaacoders.com>
2024-06-27 16:54:48 +0530

    making changes in feature branch

--- public/js/FormHandler.js
+++ public/js/FormHandler.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     form.addEventListener('submit', async (e) => {
         e.preventDefault(); // prevent loading page onsubmit
         const formData = new FormData(form);
-
+        // new changes
+        console.log(formData);
         // Display an info toast with no title
         toastr.success('Form submitted successfully')
         // alert('form submitted successfully');
